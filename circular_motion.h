5e09420c8969cb4e0b4e47820c7fbee65285feef
Puru0909 <jhapurushottam03@gmail.com>
2024-09-15 04:24:37 +0000

    sixth commit

---
+++ circular_motion.h
@@ -0,0 +1,10 @@
+// circular_motion.h
+#ifndef CIRCULAR_MOTION_H
+#define CIRCULAR_MOTION_H
+
+// Function declarations for circular motion calculations
+double calculateCentripetalForce(double mass, double velocity, double radius);
+double calculateCentripetalAcceleration(double velocity, double radius);
+double calculateAngularVelocity(double velocity, double radius);
+
+#endif // CIRCULAR_MOTION_H
